a47f850387a0b3e8f36d331b5b80ab69ec45e785
One Punch <hchang004@gmail.com>
2020-03-10 20:50:59 -0500

    update

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -2,10 +2,10 @@ const path = require('path');
 
 module.exports = function(app) {
   app.get('/survey', function(req, res) {
-    res.sendFile(path.join(__dirname, '/../public/survey.html'));
+    res.sendFile(path.join(__dirname, 'survey.html'));
   });
 
   app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, '/../public/home.html'));
+    res.sendFile(path.join(__dirname, 'home.html'));
   });
 };
