57880c03f233a8d6abaf28f5428e5390a7032563
One Punch <hchang004@gmail.com>
2020-03-12 22:30:59 -0500

    add logic and form validation

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,15 +1,65 @@
+//Preset friends objects
+
 let friends = [
   {
-    name: 'Ken Smith',
+    name: 'Mason Jones',
     photo:
-      'https://images.unsplash.com/photo-1542362567-b07e54358753?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
+      'https://images.unsplash.com/photo-1551026081-ae81ac20c552?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
     scores: [2, 3, 4, 1, 5, 3, 2, 4, 2, 3]
   },
   {
-    name: 'Jane Young',
+    name: 'Leo Smith',
     photo:
-      'https://images.unsplash.com/photo-1441974231531-c6227db76b6e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
+      'https://images.unsplash.com/photo-1536303158031-c868b371399f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
     scores: [2, 2, 2, 3, 2, 1, 3, 1, 4, 3]
+  },
+  {
+    name: 'Liam Garcia',
+    photo:
+      'https://images.unsplash.com/photo-1583356016370-fce4ca860a8c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
+    scores: [3, 5, 3, 2, 1, 2, 4, 1, 5, 2]
+  },
+  {
+    name: 'Noah Edwards',
+    photo:
+      'https://images.unsplash.com/photo-1583180383707-2aec49ed6fdc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
+    scores: [3, 1, 3, 2, 1, 3, 2, 1, 3, 2]
+  },
+  {
+    name: 'Lucas Brown',
+    photo:
+      'https://images.unsplash.com/photo-1580851935978-f6b4e359da3f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
+    scores: [1, 2, 3, 4, 5, 2, 3, 3, 2, 1]
+  },
+  {
+    name: 'Mia Lopez',
+    photo:
+      'https://images.unsplash.com/photo-1536766768598-e09213fdcf22?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
+    scores: [2, 3, 1, 3, 3, 3, 2, 4, 1, 3]
+  },
+  {
+    name: 'Olivia Thompson',
+    photo:
+      'https://images.unsplash.com/photo-1492447166138-50c3889fccb1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
+    scores: [1, 4, 5, 5, 1, 1, 2, 3, 4, 5]
+  },
+  {
+    name: 'Luna Lee',
+    photo:
+      'https://images.unsplash.com/photo-1583523196389-95f5c7f2fa3f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
+    scores: [4, 1, 5, 1, 3, 2, 3, 4, 4, 5]
+  },
+  {
+    name: 'Amelia Park',
+    photo:
+      'https://images.unsplash.com/photo-1579997701494-2045b34f9def?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
+    scores: [5, 1, 3, 4, 4, 3, 2, 1, 5, 3]
+  },
+  {
+    name: 'Logan Wang',
+    photo:
+      'https://images.unsplash.com/photo-1545058041-bc5218183a5e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
+    scores: [2, 3, 4, 5, 2, 1, 3, 1, 2, 3]
   }
 ];
 
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -6,10 +6,48 @@ module.exports = function(app) {
   });
 
   app.post('/api/friends', function(req, res) {
-    let newFriend = req.body;
+    //grab new friend score from the body
+    let newFriendScores = req.body.scores;
 
-    console.log(newFriend);
-    friends.push(newFriend);
-    res.json(newFriend);
+    let bestFriend;
+    let difference = 0;
+
+    //loop through friends array
+    for (var i = 0; i < friends.length; i++) {
+      //loop through scores in friend array
+      for (var k = 0; k < friends[i].scores.length; k++) {
+        //calculate the difference between each individual firends score and the newFriendScores
+        //Math.abs() to prevent negative number
+        //preseInt() convert string in array to number
+        difference += Math.abs(
+          parseInt(friends[i].scores[k]) - parseInt(newFriendScores[k])
+        );
+        // console.log('innerloop');
+
+        // console.log(difference);
+      }
+
+      //conditional statement to compare the difference to find the most compatible friend from the friends array
+      //least difference is the most compatible friend
+      if (i === 0) {
+        bestFriend = friends[i];
+
+        //adding difference key to object
+        bestFriend.difference = difference;
+      } else if (difference < bestFriend.difference) {
+        bestFriend = friends[i];
+        bestFriend.difference = difference;
+      }
+
+      //reseting difference
+      difference = 0;
+    }
+
+    //return most compatible friend as json
+
+    res.json(bestFriend);
+
+    //push most compatible friend into friends array
+    friends.push(req.body);
   });
 };
